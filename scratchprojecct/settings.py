f24d73e498e9fc864bbed50b8f5a45d13d3282f0
Arslan <4rs1ck@gmail.com>
2021-05-11 19:11:13 +0600

    asd

--- scratchprojecct/settings.py
+++ scratchprojecct/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,7 +98,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 25,
 }
 
+SIMPLE_JWT = {
 
+    'ACCESS_TOKEN_LIFETIME':timedelta(days=1)
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=10)
+}
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
