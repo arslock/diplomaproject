53ab0c11a3152648b61772174dddcfe9367f35f5
Arslan <4rs1ck@gmail.com>
2021-05-13 20:04:29 +0600

    asd

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,7 +20,7 @@ path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-path('profile/', ProfileViewSet.as_view({'get': 'profile', 'put': 'update'})),
+path('profile/', ProfileViewSet.as_view({'get': 'profile', 'put': 'update', 'patch':'update'})),
 # path('student-profile/<int:pk>', UserProfileView.as_view()),
 path('user-profile/<int:user_id>', UserProfileView.as_view()),
 path('change/password/', ChangePasswordViewSet.as_view({'post': 'change'})),
